c4c9be0b4a933114eb6e7532b88db8cdbde213b9
jomvlmrs-official <jomvlmrs09@gmail.com>
2025-01-25 18:51:07 +0800

    Delete ScriptForge directory

--- ScriptForge/scriptforge_schema.py
+++
@@ -1,9 +0,0 @@
-# Minecraft Bedrock Edition block schema
-scriptforge_schema = {
-    "type": "object",
-    "properties": {
-        "block_identifier": {"type": "string"},
-        "states": {"type": "object"},
-    },
-    "required": ["block_identifier", "states"]
-}
